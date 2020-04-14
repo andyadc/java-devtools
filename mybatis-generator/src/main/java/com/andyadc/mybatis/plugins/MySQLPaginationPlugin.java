1f283486ea8b0979904dc6d3c73aa5de3f100004
andaicheng <andy.an@99bill.com>
2020-04-14 15:04:27 +0800

    mybatis page generator

---
+++ mybatis-generator/src/main/java/com/andyadc/mybatis/plugins/MySQLPaginationPlugin.java
@@ -0,0 +1,106 @@
+package com.andyadc.mybatis.plugins;
+
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.PluginAdapter;
+import org.mybatis.generator.api.dom.java.Field;
+import org.mybatis.generator.api.dom.java.FullyQualifiedJavaType;
+import org.mybatis.generator.api.dom.java.JavaVisibility;
+import org.mybatis.generator.api.dom.java.Method;
+import org.mybatis.generator.api.dom.java.Parameter;
+import org.mybatis.generator.api.dom.java.PrimitiveTypeWrapper;
+import org.mybatis.generator.api.dom.java.TopLevelClass;
+import org.mybatis.generator.api.dom.xml.Attribute;
+import org.mybatis.generator.api.dom.xml.TextElement;
+import org.mybatis.generator.api.dom.xml.XmlElement;
+
+import java.util.List;
+
+/**
+ * 自定义在Example中生成分页语句
+ * 只能在程序中运行, 不能使用maven插件方式执行
+ */
+public class MySQLPaginationPlugin extends PluginAdapter {
+
+    public MySQLPaginationPlugin() {
+    }
+
+    @Override
+    public boolean validate(List<String> list) {
+        return true;
+    }
+
+    /**
+     * 为每个Example类添加limit和offset属性已经set、get方法
+     */
+    @Override
+    public boolean modelExampleClassGenerated(TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+
+        PrimitiveTypeWrapper integerWrapper = FullyQualifiedJavaType.getIntInstance().getPrimitiveTypeWrapper();
+
+        Field limit = new Field("limit", integerWrapper);
+//        limit.setName("limit");
+//        limit.setType(integerWrapper);
+        limit.setVisibility(JavaVisibility.PRIVATE);
+        topLevelClass.addField(limit);
+
+        Method setLimit = new Method("setLimit");
+//        setLimit.setName("setLimit");
+        setLimit.setVisibility(JavaVisibility.PUBLIC);
+        setLimit.addParameter(new Parameter(integerWrapper, "limit"));
+        setLimit.addBodyLine("this.limit = limit;");
+        topLevelClass.addMethod(setLimit);
+
+        Method getLimit = new Method("getLimit");
+//        getLimit.setName("getLimit");
+        getLimit.setVisibility(JavaVisibility.PUBLIC);
+        getLimit.setReturnType(integerWrapper);
+        getLimit.addBodyLine("return limit;");
+        topLevelClass.addMethod(getLimit);
+
+        Field offset = new Field("offset", integerWrapper);
+//        offset.setName("offset");
+//        offset.setType(integerWrapper);
+        offset.setVisibility(JavaVisibility.PRIVATE);
+        topLevelClass.addField(offset);
+
+        Method setOffset = new Method("setOffset");
+//        setOffset.setName("setOffset");
+        setOffset.setVisibility(JavaVisibility.PUBLIC);
+        setOffset.addParameter(new Parameter(integerWrapper, "offset"));
+        setOffset.addBodyLine("this.offset = offset;");
+        topLevelClass.addMethod(setOffset);
+
+        Method getOffset = new Method("getOffset");
+//        getOffset.setName("getOffset");
+        getOffset.setVisibility(JavaVisibility.PUBLIC);
+        getOffset.setReturnType(integerWrapper);
+        getOffset.addBodyLine("return offset;");
+        topLevelClass.addMethod(getOffset);
+
+        return true;
+    }
+
+    /**
+     * 为Mapper.xml的selectByExample添加limit
+     */
+    @Override
+    public boolean sqlMapSelectByExampleWithoutBLOBsElementGenerated(XmlElement element, IntrospectedTable introspectedTable) {
+
+        XmlElement ifLimitNotNullElement = new XmlElement("if");
+        ifLimitNotNullElement.addAttribute(new Attribute("test", "limit != null"));
+
+        XmlElement ifOffsetNotNullElement = new XmlElement("if");
+        ifOffsetNotNullElement.addAttribute(new Attribute("test", "offset != null"));
+        ifOffsetNotNullElement.addElement(new TextElement("limit ${offset}, ${limit}"));
+        ifLimitNotNullElement.addElement(ifOffsetNotNullElement);
+
+        XmlElement ifOffsetNullElement = new XmlElement("if");
+        ifOffsetNullElement.addAttribute(new Attribute("test", "offset == null"));
+        ifOffsetNullElement.addElement(new TextElement("limit ${limit}"));
+        ifLimitNotNullElement.addElement(ifOffsetNullElement);
+
+        element.addElement(ifLimitNotNullElement);
+
+        return true;
+    }
+}
